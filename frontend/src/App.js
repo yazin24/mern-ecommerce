81d0152a15931545cffc94e44979a858e5386d56
yazin24 <yazinconiendo@gmail.com>
2023-03-27 11:17:12 +0800

    Begin Ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Drift Shop</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
