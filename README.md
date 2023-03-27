07a27349fddd36bc9029cae34e824ac114254d62
yazin24 <yazinconiendo@gmail.com>
2023-03-27 12:20:31 +0800

    .

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Creating React app
 2. Configuring all necessary tools and extensions to be use
+3. Making the product card
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +7,27 @@ function App() {
       <header>
         <a href="/">Drift Shop</a>
       </header>
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>₱{product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
